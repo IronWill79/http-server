e98ffe85e623c71da1813a88537e2b3b71291107
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-10-05 10:00:54 +1000

    Add internal/server
    - Create basic http server

---
+++ internal/server/server.go
@@ -0,0 +1,51 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"sync/atomic"
+)
+
+type Server struct {
+	closed   atomic.Bool
+	listener net.Listener
+}
+
+func Serve(port int) (*Server, error) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return nil, err
+	}
+	s := Server{
+		listener: listener,
+	}
+	go func() {
+		s.listen()
+	}()
+	return &s, nil
+}
+
+func (s *Server) Close() error {
+	s.closed.Store(true)
+	return s.listener.Close()
+}
+
+func (s *Server) listen() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go func(c net.Conn) {
+			if s.closed.Load() {
+				return
+			}
+			s.handle(c)
+		}(conn)
+	}
+}
+
+func (s *Server) handle(conn net.Conn) {
+	conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 13\r\n\r\nHello World!\n"))
+}
