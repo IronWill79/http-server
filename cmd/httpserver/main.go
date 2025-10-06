0e0dc38b1be9fea1bc65dba9103c9dbb5f409782
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2025-10-06 10:14:33 +1000

    Update cmd/httpserver
    - Add handler function implementation

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,18 +1,38 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/IronWill79/http-server/internal/request"
 	"github.com/IronWill79/http-server/internal/server"
 )
 
 const port = 42069
 
+func handler(w io.Writer, req *request.Request) *server.HandlerError {
+	switch req.RequestLine.RequestTarget {
+	case "/yourproblem":
+		return &server.HandlerError{
+			Status:  400,
+			Message: "Your problem is not my problem\n",
+		}
+	case "/myproblem":
+		return &server.HandlerError{
+			Status:  500,
+			Message: "Woopsie, my bad\n",
+		}
+	default:
+		w.Write([]byte("All good, frfr\n"))
+	}
+	return nil
+}
+
 func main() {
-	server, err := server.Serve(port)
+	server, err := server.Serve(port, handler)
 	if err != nil {
 		log.Fatalf("Error starting server: %v", err)
 	}
